89c68a858a1f64754af810483fa1b6a303a11ccf
alojen <alojen@example.com>
2026-04-21 14:27:13 +0000

    fix: resolve gosec unhandled error and log injection

--- main.go
+++ main.go
@@ -90,11 +90,12 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	// #nosec G706
+    log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
